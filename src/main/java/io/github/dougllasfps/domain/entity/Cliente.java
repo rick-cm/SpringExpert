35444c9ba3606df930876dc8616653e5429df9e7
Rick <rick_mmartins@hotmail.com>
2020-09-27 12:21:05 -0300

    primeiro commit

---
+++ src/main/java/io/github/dougllasfps/domain/entity/Cliente.java
@@ -0,0 +1,43 @@
+package io.github.dougllasfps.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "cliente" )
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 100)
+    @NotEmpty(message = "Campo nome é obrigatório.")
+    private String nome;
+
+    @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório.")
+    @CPF(message = "Informe um CPF válido.")
+    private String cpf;
+
+    @JsonIgnore
+    @OneToMany( mappedBy = "cliente" , fetch = FetchType.LAZY )
+    private Set<Pedido> pedidos;
+
+   public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+}
